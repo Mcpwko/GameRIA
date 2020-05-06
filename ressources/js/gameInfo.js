1600058aeb511c6c4c4881ce4d11f60d08c970df
Jacket28 <48347504+Jacket28@users.noreply.github.com>
2020-05-06 13:15:08 +0200

    Intégration du menu principal du jeu sur le site

---
+++ ressources/js/gameInfo.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+
+    document.getElementById("regleJeu").style.display = "none";
+    document.getElementById("jeu").style.display = "none";
+    document.getElementById("scorediv").style.display = "none";
+
+}
